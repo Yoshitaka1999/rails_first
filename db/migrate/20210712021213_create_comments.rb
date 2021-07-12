9c41ed149b8926a00ad40a1834cccb3a5a7c71d1
加藤義隆 <yoshitakakato@katouyoshitakanoMacBook-Air.local>
2021-07-12 15:54:55 +0900

    コメント機能作成

--- db/migrate/20210712021213_create_comments.rb
+++ db/migrate/20210712021213_create_comments.rb
@@ -2,8 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.string :body
-      t.string :post
-      t.references :foreign_key
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
